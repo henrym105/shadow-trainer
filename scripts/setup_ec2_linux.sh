7a84d20cc7408c7dbddf2862fc772c686fa75fbf
henry marquardt <henrymarquardt1@gmail.com>
2025-07-07 20:15:24 +0000

    Add scripts for NGINX configuration and testing, including setup for EC2

--- scripts/setup_ec2_linux.sh
+++ scripts/setup_ec2_linux.sh
@@ -1,7 +1,9 @@
 
-./install_setup_uv.sh
+./scripts/install_setup_uv.sh
 
 sudo yum update -y
 
 # Opencv utils
 sudo yum install -y mesa-libGL mesa-libGL-devel libXext libSM libXrender
+
+source .venv/bin/activate
